e46fec40e04335014f92f7089882605ccae4dc7b
Christopher Maduka <61519473+Itzmadukz@users.noreply.github.com>
2024-03-29 20:48:03 -0400

    Add: deployment cors origin \n Remove: api folder

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -2,8 +2,8 @@
 //TODO: Add client server url
 const allowedOrigins = [ 
     'http://localhost:3000',
-    'http://localhost:5173'
-
+    'http://localhost:5173',
+    'https://bargain-git-hi-fi-lraes-projects.vercel.app'
 ]
 
 module.exports = allowedOrigins
