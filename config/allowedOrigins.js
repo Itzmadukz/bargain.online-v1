b98884fc4298bfd31507b0b0f731580b6bb67fa8
Christopher Maduka <61519473+Itzmadukz@users.noreply.github.com>
2024-04-03 13:32:20 -0400

    Add: Cors deployment link

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -3,7 +3,7 @@
 const allowedOrigins = [ 
     'http://localhost:3000',
     'http://localhost:5173',
-    'https://bargain-m3q1k4lfm-lraes-projects.vercel.app'
+    'https://bargain-cyan.vercel.app'
 ]
 
 module.exports = allowedOrigins
